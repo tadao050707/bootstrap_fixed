1316c8cf386227e664047917b874e98f8cd698dd
atsushi oshima <aboutagial@yahoo.co.jp>
2021-12-23 22:43:47 +0900

    guest_login機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   root 'tops#index'
   resources :fixed_costs
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
