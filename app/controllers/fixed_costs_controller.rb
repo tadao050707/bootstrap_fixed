e6d84ad12a776589827cd810be2c107180844343
atsushi oshima <aboutagial@yahoo.co.jp>
2022-01-12 20:47:53 +0900

    index合計金額_検証のため

--- app/controllers/fixed_costs_controller.rb
+++ app/controllers/fixed_costs_controller.rb
@@ -3,28 +3,87 @@ class FixedCostsController < ApplicationController
   before_action :set_fixed_cost, only: %i[ edit update destroy ]
 
   def index
-    # @fixed_costs = current_user.fixed_costs.includes(:user)
-    # @total_costs = @fixed_costs.sum(:payment)
-    # @monthly_view = false
-    # @monthly_view = params[:monthly_view] if params[:monthly_view].present?
+    # @costs = FixedCost.group(:user_id).sum(:payment)
 
-    @users = User.all
-    @fixed_costs = FixedCost.includes(:user)
-    @costs = FixedCost.group(:user_id).sum(:payment)
+    ＠users = User.includes(:fixed_costs)
+    # @fixed_costs = FixedCost.includes(:user)
+    # @fixed_costs = users.fixed_costs.includes(:user)
+    total_annual = []
+    total_monthly = []
+    @users.each do |user|
+      user.fixed_costs.each do |fixed_cost|
+        if fixed_cost.monthly_annual == annual
+          total_annual << fixed_cost.payment
+          total_monthly << monthly_payment(fixed_cost.payment)
+        elsif fixed_cost.monthly_annual == monthly
+          total_annual << annual_payment(fixed_cost.payment)
+          total_monthly << fixed_cost.payment
+        end
+
+      end
+
+    end
+    @total_annual_costs = total_annual.sum
+    @total_monthly_costs = total_monthly.sum
+
+
   end
+  #
+  #   if params[:monthly_view].nil?
+  #     @monthly_view = "true"
+  #   else
+  #     @monthly_view = params[:monthly_view]
+  #   end
+  #
+  #   # -----monthly_annualの金額を月額に直して、合計して、出力する
+  #   total_annual = []
+  #   total_monthly = []
+  #   @fixed_costs.each do |fixed_cost|
+  #     if @monthly_view == "true" && fixed_cost.monthly_annual == "annual"
+  #       total_monthly << fixed_cost.payment / 12
+  #     elsif @monthly_view == "true" && fixed_cost.monthly_annual == "monthly"
+  #       total_monthly << fixed_cost.payment
+  #     elsif @monthly_view == "false" && fixed_cost.monthly_annual == "monthly"
+  #       total_annual << fixed_cost.payment * 12
+  #     else
+  #       total_annual << fixed_cost.payment
+  #     end
+  #   end
+  #   @total_monthly_costs = total_monthly.sum
+  #   @total_annual_costs = total_annual.sum
+  #
 
   def show
     # redirect_to fixed_costs_path if @fixed_cost.blank?
     user = User.find_by(id: params[:id])
+    # user = User.find(params[:id])
     @fixed_costs = user.fixed_costs.includes(:user)
-    @total_costs = @fixed_costs.sum(:payment)
-    # @monthly_view = true
-    # binding.irb
+
     if params[:monthly_view].nil?
       @monthly_view = "true"
     else
       @monthly_view = params[:monthly_view]
     end
+
+    # -----monthly_annualの金額を月額に直して、合計して、出力する
+    total_annual = []
+    total_monthly = []
+    @fixed_costs.each do |fixed_cost|
+      if @monthly_view == "true" && fixed_cost.monthly_annual == "annual"
+        # total_monthly << fixed_cost.payment / 12
+        total_monthly << monthly_payment(fixed_cost)
+      elsif @monthly_view == "true" && fixed_cost.monthly_annual == "monthly"
+        total_monthly << fixed_cost.payment
+      elsif @monthly_view == "false" && fixed_cost.monthly_annual == "monthly"
+        # total_annual << fixed_cost.payment * 12
+        total_annual << annual_payment(fixed_cost)
+      else
+        total_annual << fixed_cost.payment
+      end
+    end
+    @total_monthly_costs = total_monthly.sum
+    @total_annual_costs = total_annual.sum
+
   end
 
   def new
@@ -35,7 +94,7 @@ class FixedCostsController < ApplicationController
   def create
     @fixed_cost = current_user.fixed_costs.build(fixed_cost_params)
     if @fixed_cost.save
-      redirect_to fixed_costs_path, notice: "「」登録しました"
+      redirect_to fixed_cost_path(current_user), notice: "新しく「#{@fixed_cost}」を登録しました"
     else
       @categories = current_user.categories.includes(:user)
       render :new
@@ -48,14 +107,14 @@ class FixedCostsController < ApplicationController
 
   def update
     @fixed_cost.update(fixed_cost_params)
-    redirect_to fixed_costs_path, notice: "変更しました"
+    redirect_to fixed_cost_path(current_user), notice: "変更しました"
     render :edit if @fixed_cost.invalid?
   end
 
   def destroy
     if @fixed_cost.destroy
       flash[:success] = '削除しました。'
-      redirect_to fixed_costs_path
+      redirect_to fixed_cost_path(current_user)
     else
       flash[:danger] = '削除に失敗しました。'
       redirect_to user_path(current_user)
