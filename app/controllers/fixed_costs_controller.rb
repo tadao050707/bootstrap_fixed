15e294fc57da1464d15d773cafd6b025080f4352
atsushi oshima <aboutagial@yahoo.co.jp>
2022-01-12 22:55:27 +0900

    index合計金額実装

--- app/controllers/fixed_costs_controller.rb
+++ app/controllers/fixed_costs_controller.rb
@@ -3,58 +3,10 @@ class FixedCostsController < ApplicationController
   before_action :set_fixed_cost, only: %i[ edit update destroy ]
 
   def index
-    # @costs = FixedCost.group(:user_id).sum(:payment)
-
-    ＠users = User.includes(:fixed_costs)
-    # @fixed_costs = FixedCost.includes(:user)
-    # @fixed_costs = users.fixed_costs.includes(:user)
-    total_annual = []
-    total_monthly = []
-    @users.each do |user|
-      user.fixed_costs.each do |fixed_cost|
-        if fixed_cost.monthly_annual == annual
-          total_annual << fixed_cost.payment
-          total_monthly << monthly_payment(fixed_cost.payment)
-        elsif fixed_cost.monthly_annual == monthly
-          total_annual << annual_payment(fixed_cost.payment)
-          total_monthly << fixed_cost.payment
-        end
-
-      end
-
-    end
-    @total_annual_costs = total_annual.sum
-    @total_monthly_costs = total_monthly.sum
-
-
+    @users = User.includes(:fixed_costs)
   end
-  #
-  #   if params[:monthly_view].nil?
-  #     @monthly_view = "true"
-  #   else
-  #     @monthly_view = params[:monthly_view]
-  #   end
-  #
-  #   # -----monthly_annualの金額を月額に直して、合計して、出力する
-  #   total_annual = []
-  #   total_monthly = []
-  #   @fixed_costs.each do |fixed_cost|
-  #     if @monthly_view == "true" && fixed_cost.monthly_annual == "annual"
-  #       total_monthly << fixed_cost.payment / 12
-  #     elsif @monthly_view == "true" && fixed_cost.monthly_annual == "monthly"
-  #       total_monthly << fixed_cost.payment
-  #     elsif @monthly_view == "false" && fixed_cost.monthly_annual == "monthly"
-  #       total_annual << fixed_cost.payment * 12
-  #     else
-  #       total_annual << fixed_cost.payment
-  #     end
-  #   end
-  #   @total_monthly_costs = total_monthly.sum
-  #   @total_annual_costs = total_annual.sum
-  #
 
   def show
-    # redirect_to fixed_costs_path if @fixed_cost.blank?
     user = User.find_by(id: params[:id])
     # user = User.find(params[:id])
     @fixed_costs = user.fixed_costs.includes(:user)
@@ -64,26 +16,6 @@ class FixedCostsController < ApplicationController
     else
       @monthly_view = params[:monthly_view]
     end
-
-    # -----monthly_annualの金額を月額に直して、合計して、出力する
-    total_annual = []
-    total_monthly = []
-    @fixed_costs.each do |fixed_cost|
-      if @monthly_view == "true" && fixed_cost.monthly_annual == "annual"
-        # total_monthly << fixed_cost.payment / 12
-        total_monthly << monthly_payment(fixed_cost)
-      elsif @monthly_view == "true" && fixed_cost.monthly_annual == "monthly"
-        total_monthly << fixed_cost.payment
-      elsif @monthly_view == "false" && fixed_cost.monthly_annual == "monthly"
-        # total_annual << fixed_cost.payment * 12
-        total_annual << annual_payment(fixed_cost)
-      else
-        total_annual << fixed_cost.payment
-      end
-    end
-    @total_monthly_costs = total_monthly.sum
-    @total_annual_costs = total_annual.sum
-
   end
 
   def new
@@ -94,7 +26,8 @@ class FixedCostsController < ApplicationController
   def create
     @fixed_cost = current_user.fixed_costs.build(fixed_cost_params)
     if @fixed_cost.save
-      redirect_to fixed_cost_path(current_user), notice: "新しく「#{@fixed_cost}」を登録しました"
+      # binding.pry
+      redirect_to fixed_cost_path(current_user), notice: "新しく「#{@fixed_cost.categories.map(&:cat_name)}」を登録しました"
     else
       @categories = current_user.categories.includes(:user)
       render :new
